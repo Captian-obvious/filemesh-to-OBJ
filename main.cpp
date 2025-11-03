d4139c49296dd31bc71accc54de1ad304b45b001
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-11-03 17:37:15 -0500

    initial structure

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+typedef unsigned char byte;
+typedef signed char sbyte;
+
+typedef struct {
+    unsigned short sizeof_mesh2Head;
+    byte sizeof_mesh2Vertex;
+    byte sizeof_mesh2Face;
+
+    uint vert_cnt;
+    uint face_cnt;
+} mesh2Head;
+
+typedef struct {
+    float px,py,pz;
+    float nx,ny,nz;
+    float tu,tv;
+    sbyte tx,ty,tz,ts;
+    byte r,g,b,a; // THIS IS NOT ALWAYS INCLUDED!
+} mesh2Vertex;
+typedef struct {
+    float px,py,pz;
+    float nx,ny,nz;
+    float tu,tv;
+    sbyte tx,ty,tz,ts;
+} mesh2VertexNoColor;
+
+typedef struct {
+    mesh2Head header;
+} mesh2;
+
+int main(int argc,char** argv){
+    if (argc>1){
+        // adding soon
+    };
+};
