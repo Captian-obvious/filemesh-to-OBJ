3e63544df25bb6b40ce5844809da368871a61c47
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-11-03 22:06:32 -0500

    h

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ All FileMesh versions start with "version (version)\n", INCLUDING binary ones
 typedef unsigned char byte;
 typedef signed char sbyte;
 // support soon: "1.00", "1.01"
-const char* supportedVersions[] = {"2.00","3.00","3.01"};
+const char* supportedVersions[] = {"2.00","3.00","3.01","4.00","4.01"};
 typedef struct {
     unsigned short sizeof_mesh2Head;
     byte sizeof_meshVertex;
