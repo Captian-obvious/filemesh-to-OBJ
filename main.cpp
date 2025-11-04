7a2cbd616e55a97c17d185e07ebade7c6d2eeaee
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-11-04 12:53:43 -0500

    offsets

--- main.cpp
+++ main.cpp
@@ -98,12 +98,13 @@ std::string convert_to_obj(mesh2& mesh){
 };
 std::string convert_to_obj(mesh3& mesh){
     std::string objData="# Generated from FileMesh v3.00/v3.01\n";
+    uint mainOffset=mesh.header.lod_offsets[0];
     for (uint i=0;i<mesh.header.vert_cnt;i++){
         objData+="v "+std::to_string(mesh.verts[i].px)+" "+std::to_string(mesh.verts[i].py)+" "+std::to_string(mesh.verts[i].pz)+"\n";
         objData+="vn "+std::to_string(mesh.verts[i].nx)+" "+std::to_string(mesh.verts[i].ny)+" "+std::to_string(mesh.verts[i].nz)+"\n";
         objData+="vt "+std::to_string(mesh.verts[i].tu)+" "+std::to_string(mesh.verts[i].tv)+"\n";
     };
-    for (uint i=0;i<mesh.header.face_cnt;i++){
+    for (uint i=0;i<mainOffset;i++){
         objData+="f "+std::to_string(mesh.faces[i].a+1)+"/"+std::to_string(mesh.faces[i].a+1)+"/"+std::to_string(mesh.faces[i].a+1)+" "+
                           std::to_string(mesh.faces[i].b+1)+"/"+std::to_string(mesh.faces[i].b+1)+"/"+std::to_string(mesh.faces[i].b+1)+" "+
                           std::to_string(mesh.faces[i].c+1)+"/"+std::to_string(mesh.faces[i].c+1)+"/"+std::to_string(mesh.faces[i].c+1)+"\n";
