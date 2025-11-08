16e2e16b28da0ab22ef2ef2d3251dc1b7c2c3b2e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-11-07 19:38:51 -0500

    last test before we check convert_to_obj

--- main.cpp
+++ main.cpp
@@ -521,6 +521,7 @@ int main(int argc,char** argv){
         readBytes=fread(mesh.bone_names,mesh.header.sizeof_bone_names,1,fd);
         mesh.subsets=new meshSubset[mesh.header.subset_cnt];
         readBytes=fread(mesh.subsets,sizeof(meshSubset),mesh.header.subset_cnt,fd);
+        print_info("we got here");
         std::string objData=convert_to_obj(mesh,preserve_LOD);
         if (!no_output){
             if (argc>=3){
